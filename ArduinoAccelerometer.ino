1063eac12b0c1c90b2b0fc787577e3bd1766ed89
Jeeson Johnson <44068823+jeesonjohnson@users.noreply.github.com>
2019-02-09 21:10:16 +0000

    Update ArduinoAccelerometer.ino

--- ArduinoAccelerometer.ino
+++ ArduinoAccelerometer.ino
@@ -1,86 +1,63 @@
-#include <CurieIMU.h>
-#include <MadgwickAHRS.h>
 
-Madgwick filter;
-unsigned long microsPerReading, microsPrevious;
-float accelScale, gyroScale;
+/*
+ * Copyright (c) 2016 Intel Corporation.  All rights reserved.
+ * See the bottom of this file for the license terms.
+ */
+
+/*
+   This sketch example demonstrates how the BMI160 on the
+   Intel(R) Curie(TM) module can be used to read accelerometer data
+*/
+
+#include "CurieIMU.h"
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // initialize Serial communication
+  while (!Serial);    // wait for the serial port to open
 
-  // start the IMU and filter
+  // initialize device
+  Serial.println("Initializing IMU device...");
   CurieIMU.begin();
-  CurieIMU.setGyroRate(25);
-  CurieIMU.setAccelerometerRate(25);
-  filter.begin(25);
 
   // Set the accelerometer range to 2G
-  CurieIMU.setAccelerometerRange(2);
-  // Set the gyroscope range to 250 degrees/second
-  CurieIMU.setGyroRange(250);
-
-  // initialize variables to pace updates to correct rate
-  microsPerReading = 1000000 / 25;
-  microsPrevious = micros();
+  CurieIMU.setAccelerometerRange(20);
 }
 
 void loop() {
-  int aix, aiy, aiz;
-  int gix, giy, giz;
-  float ax, ay, az;
-  float gx, gy, gz;
-  float roll, pitch, heading;
-  unsigned long microsNow;
+  float ax, ay, az;   //scaled accelerometer values
 
-  // check if it's time to read data and update the filter
-  microsNow = micros();
-  if (microsNow - microsPrevious >= microsPerReading) {
+  // read accelerometer measurements from device, scaled to the configured range
+  CurieIMU.readAccelerometerScaled(ax, ay, az);
+  delay(250);
 
-    // read raw data from CurieIMU
-    CurieIMU.readMotionSensor(aix, aiy, aiz, gix, giy, giz);
-
-    // convert from raw data to gravity and degrees/second units
-    ax = convertRawAcceleration(aix);
-    ay = convertRawAcceleration(aiy);
-    az = convertRawAcceleration(aiz);
-    gx = convertRawGyro(gix);
-    gy = convertRawGyro(giy);
-    gz = convertRawGyro(giz);
-
-    // update the filter, which computes orientation
-    filter.updateIMU(gx, gy, gz, ax, ay, az);
-
-    // print the heading, pitch and roll
-    roll = filter.getRoll();
-    pitch = filter.getPitch();
-    heading = filter.getYaw();
-    Serial.print(heading);
-    Serial.print(";");
-    Serial.print(pitch);
-    Serial.print(";");
-    Serial.println(roll);
-
-    // increment previous time, so we keep proper pace
-    microsPrevious = microsPrevious + microsPerReading;
-  }
-}
-
-float convertRawAcceleration(int aRaw) {
-  // since we are using 2G range
-  // -2g maps to a raw value of -32768
-  // +2g maps to a raw value of 32767
+  // display tab-separated accelerometer x/y/z values
+  Serial.print(ax);
   
-  float a = (aRaw * 2.0) / 32768.0;
-  return a;
-}
-
-float convertRawGyro(int gRaw) {
-  // since we are using 250 degrees/seconds range
-  // -250 maps to a raw value of -32768
-  // +250 maps to a raw value of 32767
+  // The /t adds a tab
+  Serial.print(";");
+  Serial.print(ay);
   
-  float g = (gRaw * 250.0) / 32768.0;
-  return g;
+
+  Serial.print(";");
+  Serial.print(az);
+  Serial.println();
 }
 
+/*
+   Copyright (c) 2016 Intel Corporation.  All rights reserved.
 
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
